b32bd97e7685d4ed15bd56781a493007fc33d085
stmoree <stmoree@gmail.com>
2017-12-20 21:56:12 -0800

    Adding starting project

---
+++ WebSite1/Scripts/app.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module("MyApp", []);
+app.controller("HomeController", function ($scope) {
+    $scope.ClanMembers = [
+        { name: 'Ace', rank: 'Co-Leader' },
+        { name: 'ima hood boi', rank: 'Co-Leader' },
+        { name: 'OptikTank', rank: 'Co-Leader' },
+        { name: 'tazer-face', rank: 'Clan noob' },
+        { name: 'Voidptr', rank: 'elder' },
+        { name: 'maryam', rank: 'Co-Leader' }];
+});
